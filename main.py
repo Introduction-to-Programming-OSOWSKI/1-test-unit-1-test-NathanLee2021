a0c224702e47523a5ec79f0a14d12dc733b18a5c
NathanLee2021 <818998@mystma.org>
2020-12-22 16:17:40 +0000

    Test #1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return (n)
+print (name("Nathan Lee"))
+
+def favorites(favColor, favNumber):
+    return (favColor, favNumber)
+print (favorites("Blue, 7"))
+
+def bigAdd(x, y, z, a, b):
+    return (x+y+z+a+b)
+print (bigAdd(1, 2, 3, 4, 5))
+
+def fourOperations(X, Y, Z, A, B):
+    return X + Y - Z * A / B
+print (fourOperations(1, 2, 3, 4, 5))
